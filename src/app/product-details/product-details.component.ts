392be15344cbc15f3204ec007b3b2cf76dd7f4e0
ddhedstrom <derek.hedstrom@alaska.gov>
2022-08-02 11:09:55 -0800

    7 files modified, 4 added

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -10,26 +10,26 @@ import { CartService } from '../cart.service';
   styleUrls: ['./product-details.component.css'],
 })
 export class ProductDetailsComponent implements OnInit {
-  product: Product | undefined;
 
   constructor(
     private route: ActivatedRoute,
     private cartService: CartService
   ) {}
 
+  product: Product | undefined;
+
   addToCart(product: Product) {
     this.cartService.addToCart(product);
     window.alert('Your product has been added to the cart!');
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
     //First get the product id from the current route.
     const routeParams = this.route.snapshot.paramMap;
     const productIdFromRoute = Number(routeParams.get('productID'));
 
     // Find the product that corrosponds with the id provided in route
-    this.product = products.find(
-      (product) => product.id === productIdFromRoute
+    this.product = products.find(product => product.id === productIdFromRoute
     );
   }
 }
